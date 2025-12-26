357332ab5b0ea4cfffb2026155a9e18d4ef6e8a1
kmgyu <lidersy961@gmail.com>
2025-12-26 18:44:55 +0900

    test: docker 환경 및 공통 API 반환 코드 추가

---
+++ backend/src/main/java/groom/backend/domain/location/dto/response/LocationResponse.java
@@ -0,0 +1,4 @@
+package groom.backend.domain.location.dto.response;
+
+public record LocationResponse(String message) {
+}
--- backend/src/main/java/groom/backend/domain/location/repository/impl/LocationRespositoryImpl.java
+++
@@ -1,6 +0,0 @@
-package groom.backend.domain.location.repository.impl;
-
-import groom.backend.domain.location.service.spec.LocationService;
-
-public class LocationRespositoryImpl implements LocationService {
-}
