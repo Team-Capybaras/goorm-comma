53d61f400252dddf836dcd61d385351bf9ab40ad
박총명 <qkrri56@naver.com>
2025-12-26 11:51:15 +0900

    build : 기초 세팅
    next js 설치
    - typescript
    - tailwindcss
    - esLint
    lint 및 prettier 설정
    zustand 설치
    react query 설치 및 provider 설정
    기본 layout 설정

---
+++ frontend/src/app/(view)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+export default function MainLayout({children}) {
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
