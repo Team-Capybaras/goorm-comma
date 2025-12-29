63ecdc5f4a3ca5230da65cd6b7d912322a6cdd0f
최원형 <cwh0607@naver.com>
2025-12-29 15:41:29 +0900

    feat: 지도 화면 노출

--- frontend/src/app/(view)/page.tsx
+++ frontend/src/app/(view)/page.tsx
@@ -1,7 +1,12 @@
+import KakaoMap from '@/components/common/KakaoMap'
+
 export default function page() {
   return (
     <>
       main입니다.
+      <div className="h-[500px] w-[400px]">
+        <KakaoMap />
+      </div>
     </>
   )
 }
