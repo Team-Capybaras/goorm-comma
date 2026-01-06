815ea0aafb7667209c114c2340b71acd2b7bf616
kmgyu <lidersy961@gmail.com>
2026-01-06 21:36:11 +0900

    feat(weather): 특정 공원에 대해 가장 최근 데이터 가져오기 기능 구현 - WeatherStatusId에 대해 postgresql 더블 PK에서 선두 컬럼이 인덱싱 됨으로 인해 순서 수정

--- backend/src/main/java/groom/backend/domain/weather/service/impl/WeatherCurrentStatusServiceImpl.java
+++ backend/src/main/java/groom/backend/domain/weather/service/impl/WeatherCurrentStatusServiceImpl.java
@@ -1,6 +1,8 @@
 package groom.backend.domain.weather.service.impl;
 
 import groom.backend.domain.weather.dto.response.WeatherStatusResponse;
+import groom.backend.domain.weather.entity.WeatherStatus;
+import groom.backend.domain.weather.mapper.WeatherMapper;
 import groom.backend.domain.weather.repository.WeatherStatusRepository;
 import groom.backend.domain.weather.service.spec.WeatherCurrentStatusService;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +13,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class WeatherCurrentStatusServiceImpl implements WeatherCurrentStatusService {
   private final WeatherStatusRepository weatherStatusRepository;
+  private final WeatherMapper weatherMapper;
 
   @Override
-  public WeatherStatusResponse getWeatherCurrentStatus() {
+  public WeatherStatusResponse getWeatherCurrentStatus(String areaCode) {
     // 특정 areaCode에 대해, dataGetTime이 가장 최신인 데이터 반환
-    return null;
+    // TODO: 데이터가 없을 경우 NoDataException 출력
+    WeatherStatus weatherStatus = weatherStatusRepository.findTopByAreaCodeOrderByDataGetTimeDesc(areaCode).orElse(null);
+
+    return weatherMapper.toWeatherDto(weatherStatus);
   }
 }
--- backend/src/main/java/groom/backend/domain/weather/service/spec/WeatherCurrentStatusService.java
+++ backend/src/main/java/groom/backend/domain/weather/service/spec/WeatherCurrentStatusService.java
@@ -6,5 +6,5 @@ import groom.backend.domain.weather.dto.response.WeatherStatusResponse;
  * 현재 시간 기준 일기예보 및 날씨를 제공하는 서비스
  */
 public interface WeatherCurrentStatusService {
-  public WeatherStatusResponse getWeatherCurrentStatus();
+  public WeatherStatusResponse getWeatherCurrentStatus(String areaCode);
 }
