98bac490d61fcb6c4ec0a53888a7e34fc7a172b2
kmgyu <lidersy961@gmail.com>
2026-01-07 16:20:13 +0900

    refactor(parking): 서비스 클래스 명칭 변경

--- backend/src/main/java/groom/backend/domain/parking/service/spec/ParkingStatusService.java
+++ backend/src/main/java/groom/backend/domain/parking/service/spec/ParkingStatusService.java
@@ -5,7 +5,7 @@ import groom.backend.domain.parking.dto.response.ParkingLocationResponse;
 /**
  * 주차장 및 전기차 충전소 등 편의시설 정보 제공 서비스
  */
-public interface ParkingService {
+public interface ParkingStatusService {
   /**
    * 전기차 충전소와 주차장의 위치 및 현재 현황 등의 요약 정보를 알려줍니다.
    */
