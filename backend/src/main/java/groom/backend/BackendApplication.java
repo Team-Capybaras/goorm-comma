8166fc2815081b664c374172d89c318f11be1789
kmgyu <lidersy961@gmail.com>
2026-01-13 17:20:05 +0900

    fix(avoidance): redis LocalDateTime 직렬화 오류 수정 - jsr310 의존성 추가 - object mapper 추가 - main 클래스에서 @EnableCaching 제거

--- backend/src/main/java/groom/backend/BackendApplication.java
+++ backend/src/main/java/groom/backend/BackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
-@EnableCaching
 @SpringBootApplication
 public class BackendApplication {
 
