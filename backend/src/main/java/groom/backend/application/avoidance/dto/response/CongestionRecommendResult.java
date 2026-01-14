3c04b243d827888bd33e1565060259828d5ce99f
kmgyu <lidersy961@gmail.com>
2026-01-14 17:14:26 +0900

    fix(avoidance): 정보 명확성을 위한 DTO 스펙 재정의 - 데이터가 부족할 시 일부 시간대가 나타나지 못할 수 있는 문제 존재, hour 필드 명시 - 시간대별 혼잡도 예측과 시간대 추천 dto를 각각 분리 - 추천 텍스트 필드 추가

--- backend/src/main/java/groom/backend/application/avoidance/dto/response/CongestionRecommendResult.java
+++ backend/src/main/java/groom/backend/application/avoidance/dto/response/CongestionRecommendResult.java
@@ -6,16 +6,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 /**
- * 모델 예측 결과
+ * 모델 예측 결과 및 추천 시간대
  * 내부 DTO
  */
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class CongestionPredResult {
+public class CongestionRecommendResult {
   private Weekday weekday;               // 기준 요일
-  private int hour;           // 추천 시간 (09-22시 사이)
-  private Integer predCongestions; // 혼잡도 예측
+  private int recommendedHour;           // 추천 시간 (09-22시 사이)
+  private String message;       // 추천 텍스트 메시지
+  private List<CongestionPredResult> predCongestions; // 혼잡도 예측
 }
