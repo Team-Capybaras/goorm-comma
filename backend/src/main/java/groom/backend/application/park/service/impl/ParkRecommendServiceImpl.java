9d3c49404c68692ca40e628a5740a965a36ccba0
kmgyu <lidersy961@gmail.com>
2026-01-16 13:50:46 +0900

    fix(park): 요구사항에 맞게 수헝 - 여유 및 보통 공원만 필터링

--- backend/src/main/java/groom/backend/application/park/service/impl/ParkRecommendServiceImpl.java
+++ backend/src/main/java/groom/backend/application/park/service/impl/ParkRecommendServiceImpl.java
@@ -66,7 +66,7 @@ public class ParkRecommendServiceImpl implements ParkRecommendService {
     // 3. 혼합 정렬 (혼잡도 → 거리 → areaCode)
     List<GetAllParksResponse.ParkInfo> sorted = parkInfos.stream()
             // 1. 혼잡도 "여유"만 필터링
-            .filter(p -> "여유".equals(p.getAreaCongestLevel()))
+            .filter(p -> ("여유".equals(p.getAreaCongestLevel()) || ("보통".equals(p.getAreaCongestLevel()))) )
             // 2. 혼합 정렬
             .sorted(mixedRecommendComparator())
             // 3. Top 5 제한
