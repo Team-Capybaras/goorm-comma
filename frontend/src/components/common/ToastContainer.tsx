e8862a0a8c396f4003639700e638f6ba5b1fa4b6
박총명 <qkrri56@naver.com>
2026-01-20 18:17:08 +0900

    feat : 공통 모달 사용 및 toast 위치 변경

--- frontend/src/components/common/ToastContainer.tsx
+++ frontend/src/components/common/ToastContainer.tsx
@@ -14,7 +14,7 @@ export default function useToast() {
   const removeToast = (id: string) => setToasts(prev => prev.filter(t => t.id !== id))
 
   const ToastContainer = (
-    <div className="fixed bottom-25 right-5 z-[999] flex flex-col gap-2">
+    <div className="fixed bottom-25 left-1/2 -translate-x-1/2 z-[999] min-w-[80vw] flex flex-col gap-2">
       {toasts.map(t => (
         <Toast
           key={t.id}
