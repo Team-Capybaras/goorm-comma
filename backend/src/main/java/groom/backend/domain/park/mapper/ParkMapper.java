a9cd624e11cba2e6d1c02a481bd98c27d5d516dc
kmgyu <lidersy961@gmail.com>
2026-01-23 14:42:12 +0900

    chore: 테스트코드 작성 중 손상된 파일 복구

---
+++ backend/src/main/java/groom/backend/domain/park/mapper/ParkMapper.java
@@ -0,0 +1,49 @@
+package groom.backend.domain.park.mapper;
+
+import groom.backend.domain.park.dto.response.GetAllParksBasicResponse;
+import groom.backend.domain.park.dto.response.GetParkSearchResponse;
+import groom.backend.domain.park.entity.Park;
+import org.springframework.stereotype.Component;
+
+/**
+ * 공원 엔티티와 DTO 간 변환을 담당하는 매퍼
+ */
+@Component
+public class ParkMapper {
+
+    /**
+     * Park 엔티티를 ParkBasicInfo DTO로 변환합니다.
+     *
+     * @param park 공원 엔티티
+     * @return 공원 기본 정보 DTO
+     */
+    public GetAllParksBasicResponse.ParkBasicInfo toParkBasicInfoDto(Park park) {
+        if (park == null) {
+            return null;
+        }
+
+        return GetAllParksBasicResponse.ParkBasicInfo.builder()
+                .areaCode(park.getAreaCode())
+                .areaName(park.getAreaName())
+                .longitude(park.getLongitude())
+                .latitude(park.getLatitude())
+                .build();
+    }
+
+    /**
+     * Park 엔티티를 GetParkSearchResponse DTO로 변환합니다.
+     *
+     * @param park 공원 엔티티
+     * @return 공원 검색 정보 DTO
+     */
+    public GetParkSearchResponse toParkSearchDto(Park park) {
+        if (park == null) {
+            return null;
+        }
+
+        return GetParkSearchResponse.builder()
+                .areaCode(park.getAreaCode())
+                .areaName(park.getAreaName())
+                .build();
+    }
+}
