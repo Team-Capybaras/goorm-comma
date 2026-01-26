66b0660733df8b601c48b8490283e099de1912c3
최원형 <cwh0607@naver.com>
2026-01-26 16:52:13 +0900

    QA: 디자인 수정

--- frontend/src/app/(view)/(main)/_component/ParkListContent.tsx
+++ frontend/src/app/(view)/(main)/_component/ParkListContent.tsx
@@ -1,20 +1,15 @@
 'use client'
 
-import ParkFilter from "@/app/(view)/(main)/_component/ParkFilter";
-import ParkCard from "@/app/(view)/(main)/_component/ParkCard";
-import {useEffect, useRef, useState} from "react";
-import {ParkInfo, ParkListWithPage} from "@/shared/types/park-types";
-import {InfiniteData, useInfiniteQuery} from "@tanstack/react-query";
-import {fetchParks} from "@/shared/libs/park-list-api";
-import ParkListCardSkeleton from "@/app/(view)/(main)/_status/ParkListCardSkeleton";
-import ErrorComponent from "@/components/ui/ErrorComponent";
+import ParkFilter from '@/app/(view)/(main)/_component/ParkFilter'
+import ParkCard from '@/app/(view)/(main)/_component/ParkCard'
+import { useEffect, useRef, useState } from 'react'
+import { ParkInfo, ParkListWithPage } from '@/shared/types/park-types'
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query'
+import { fetchParks } from '@/shared/libs/park-list-api'
+import ParkListCardSkeleton from '@/app/(view)/(main)/_status/ParkListCardSkeleton'
+import ErrorComponent from '@/components/ui/ErrorComponent'
 
-type ParksQueryKey = [
-  'parks',
-  string[],
-  string,
-  { lat: number; lng: number }
-]
+type ParksQueryKey = ['parks', string[], string, { lat: number; lng: number }]
 
 interface ParkLocationProps {
   location: {
@@ -23,47 +18,38 @@ interface ParkLocationProps {
   }
 }
 
-export default function ParkListContent({location} : ParkLocationProps) {
+export default function ParkListContent({ location }: ParkLocationProps) {
   const [activeOptions, setActiveOptions] = useState<string[]>([])
-  const [activeSort, setActiveSort] = useState<string>("BY_DISTANCE")
+  const [activeSort, setActiveSort] = useState<string>('BY_DISTANCE')
   const loadMoreRef = useRef<HTMLDivElement | null>(null)
   const SKELETON_COUNT = 3
 
   // react-qeury fetch
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    isError,
-    error
-  } = useInfiniteQuery<
-    ParkListWithPage,
-    Error,
-    InfiniteData<ParkListWithPage>,
-    ParksQueryKey,
-    string | null
-  >({
-    queryKey: ['parks', activeOptions, activeSort, {lat: location.lat, lng: location.lng}],
-    enabled: !!location?.lat && !!location?.lng,
-    queryFn: ({ pageParam }) =>
-      fetchParks({
-        pageParam,
-        activeOptions,
-        activeSort,
-        location: location!,
-      }),
-    getNextPageParam: (lastPage) =>
-      lastPage.hasNext ? lastPage.nextCursor : undefined,
-    initialPageParam: null,
-  })
+  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage, isError, error } =
+    useInfiniteQuery<
+      ParkListWithPage,
+      Error,
+      InfiniteData<ParkListWithPage>,
+      ParksQueryKey,
+      string | null
+    >({
+      queryKey: ['parks', activeOptions, activeSort, { lat: location.lat, lng: location.lng }],
+      enabled: !!location?.lat && !!location?.lng,
+      queryFn: ({ pageParam }) =>
+        fetchParks({
+          pageParam,
+          activeOptions,
+          activeSort,
+          location: location!,
+        }),
+      getNextPageParam: (lastPage) => (lastPage.hasNext ? lastPage.nextCursor : undefined),
+      initialPageParam: null,
+    })
 
-  const totalCount:number = data?.pages?.[0]?.totalCount ?? 0
-  const count:number = data?.pages?.[0]?.count ?? 0
+  const totalCount: number = data?.pages?.[0]?.totalCount ?? 0
+  const count: number = data?.pages?.[0]?.count ?? 0
 
-  const parks: ParkInfo[] =
-    data?.pages.flatMap((page) => page.parks) ?? []
+  const parks: ParkInfo[] = data?.pages.flatMap((page) => page.parks) ?? []
 
   console.log(data)
 
@@ -85,7 +71,7 @@ export default function ParkListContent({location} : ParkLocationProps) {
   }, [fetchNextPage, hasNextPage])
 
   if (isError) {
-    return <ErrorComponent className={"bg-white"} />
+    return <ErrorComponent className={'bg-white'} />
   }
 
   return (
@@ -112,31 +98,29 @@ export default function ParkListContent({location} : ParkLocationProps) {
       {parks.length > 0 ? (
         <div className="px s-5">
           <p className="mt s-4">
-            <span className="text-body-2-sb">총 {activeOptions.length !== 0 ? count : totalCount}개</span>
+            <span className="text-body-2-sb">
+              총 {activeOptions.length !== 0 ? count : totalCount}개
+            </span>
             <span className="text-body-2-m text-sub-bright">의 공원</span>
           </p>
 
-          <div className="flex flex-col gap-8 mt s-5">
+          <div className="flex flex-col gap-8 mt s-5 mb-5">
             {parks.map((park, idx) => (
               <ParkCard key={`${park.areaName}-${idx}`} data={park} />
             ))}
           </div>
 
           {/* 다음 페이지(무한스크롤)이 있을 때 */}
-          {hasNextPage && (
-            <div ref={loadMoreRef} className="h-10" />
-          )}
+          {hasNextPage && <div ref={loadMoreRef} className="h-10" />}
 
           {/* 다음 페이지(무한스크롤)을 적용 중일 때 */}
-          {isFetchingNextPage && (
-            <ParkListCardSkeleton />
-          )}
+          {isFetchingNextPage && <ParkListCardSkeleton />}
         </div>
       ) : (
         /* list가 비어있을 때 */
         !isFetching && (
           <div className="flex flex-col justify-center items-center p-5 h-70">
-            <img src="/images/icons/caution.svg" width={24} height={24} alt="주의"/>
+            <img src="/images/icons/caution.svg" width={24} height={24} alt="주의" />
             <p className="text-sub text-body-2-m mt s-3">조건에 맞는 공원이 없어요.</p>
             <p className="text-sub text-body-2-m">필터를 다시 설정해보세요.</p>
           </div>
--- frontend/src/app/(view)/(main)/_component/ParkListWithPopulation.tsx
+++ frontend/src/app/(view)/(main)/_component/ParkListWithPopulation.tsx
@@ -4,8 +4,7 @@ import { useState } from 'react' // 1. useState 불러오기
 import { useLocationStore } from '@/store/location.store'
 import ParkListContent from '@/app/(view)/(main)/_component/ParkListContent'
 import { CongestionInfoCard } from './CongestionInfoCard'
-import ParkFilterModal from "@/app/(view)/(main)/_component/ParkFilterModal";
-import {Modal} from "@/components/ui/Modal";
+import { Modal } from '@/components/ui/Modal'
 
 export default function ParkListWithPopulation() {
   const { location } = useLocationStore()
@@ -21,7 +20,7 @@ export default function ParkListWithPopulation() {
 
   return (
     <>
-      <div className="flex justify-center mb-3">
+      <div className="flex justify-center mb-1">
         <div className="w-full max-w-2xl bg-bright flex flex-col pt s-5">
           <div className="flex items-center justify-start px s-5">
             <p className="text-caption-1-m text-sub">현재 인구 흐름을 분석해 혼잡도를 표시해요.</p>
@@ -39,7 +38,7 @@ export default function ParkListWithPopulation() {
         </div>
       </div>
 
-      <Modal open={isInfoOpen} onClose={handleCloseInfo} size={"full"} >
+      <Modal open={isInfoOpen} onClose={handleCloseInfo} size={'full'}>
         <CongestionInfoCard onClose={handleCloseInfo} />
       </Modal>
     </>
