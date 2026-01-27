63f28922634c7b4722f1f34e29414de9656d80cd
최원형 <cwh0607@naver.com>
2026-01-27 14:59:27 +0900

    QA : 추천공원 같은 공원이 뜨는 경우 제거

--- frontend/src/app/(view)/detail/_components/AlternativeParkDashboard.tsx
+++ frontend/src/app/(view)/detail/_components/AlternativeParkDashboard.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import Image from "next/image";
-import AlternativeParkClient from "@/app/(view)/detail/_components/AlternativeParkClient";
-import EmblaCarousel from "@/components/common/EmblaCarousel";
-import {CONGESTION_COLOR_MAP} from "@/shared/utils/congestion-helper";
-import {api} from "@/shared/libs/axios";
-import {ParkInfo} from "@/shared/types/park-types";
-import {useEffect, useState} from "react";
-import Link from "next/link";
+import Image from 'next/image'
+import AlternativeParkClient from '@/app/(view)/detail/_components/AlternativeParkClient'
+import EmblaCarousel from '@/components/common/EmblaCarousel'
+import { CONGESTION_COLOR_MAP } from '@/shared/utils/congestion-helper'
+import { api } from '@/shared/libs/axios'
+import { ParkInfo } from '@/shared/types/park-types'
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 interface AlternativeParkDashboardProps {
   areaCode: string
@@ -15,13 +15,14 @@ interface AlternativeParkDashboardProps {
   lng: number
 }
 
-export default function AlternativeParkDashboard({areaCode, lat, lng}: AlternativeParkDashboardProps) {
+export default function AlternativeParkDashboard({
+  areaCode,
+  lat,
+  lng,
+}: AlternativeParkDashboardProps) {
   const [data, setData] = useState<ParkInfo[]>([])
 
-  const fetchData = async (
-    lat: number,
-    lng: number
-  ): Promise<ParkInfo[]> => {
+  const fetchData = async (lat: number, lng: number): Promise<ParkInfo[]> => {
     const res = await api.get('/v1/parks/recommend', {
       params: {
         base_area_code: areaCode,
@@ -30,11 +31,18 @@ export default function AlternativeParkDashboard({areaCode, lat, lng}: Alternati
       },
     })
 
-    return res.data.data.parks
+    const parks: ParkInfo[] = res.data.data.parks || []
+    const uniqueParks = parks.filter((item, index, self) => {
+      if (item.areaCode === areaCode) return false
+
+      return index === self.findIndex((t) => t.areaName === item.areaName)
+    })
+
+    return uniqueParks
   }
 
   useEffect(() => {
-    if (!location) return
+    if (!lat || !lng) return
 
     fetchData(lat, lng)
       .then(setData)
@@ -42,7 +50,7 @@ export default function AlternativeParkDashboard({areaCode, lat, lng}: Alternati
         console.error('공원 조회 실패', err)
         setData([])
       })
-  }, [location])
+  }, [lat, lng, areaCode])
 
   return (
     <div className="mt s-5 pb-8">
@@ -54,7 +62,11 @@ export default function AlternativeParkDashboard({areaCode, lat, lng}: Alternati
         <EmblaCarousel containerClassName={'gap-x-2 mr-6'}>
           <>
             {data?.map((item, i) => (
-              <Link href={`/detail/${item.areaCode}`} className={`flex-[0_0_40%] ${i == 0 ? 'ml s-5' : ''}`} key={i}>
+              <Link
+                href={`/detail/${item.areaCode}`}
+                className={`flex-[0_0_40%] ${i == 0 ? 'ml s-5' : ''}`}
+                key={i}
+              >
                 <div className="relative w-full h-[218px]">
                   <Image
                     src={item.images[0]}
@@ -68,7 +80,7 @@ export default function AlternativeParkDashboard({areaCode, lat, lng}: Alternati
 
                 <div className="flex items-center mt s-3">
                   <p className="text-body-2-sb">{item.areaName}</p>
-                  <div className="w-[3px] h-[3px] rounded-full bg-gray-300 mx s-2"/>
+                  <div className="w-[3px] h-[3px] rounded-full bg-gray-300 mx s-2" />
                   <p className="text-body-2-m text-gray-500">{item.distance}km</p>
                 </div>
 
