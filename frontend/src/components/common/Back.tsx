830010287c7acb7905e384a0a845472551a3c87c
박총명 <qkrri56@naver.com>
2026-01-27 16:57:52 +0900

    qa : 수정

--- frontend/src/components/common/Back.tsx
+++ frontend/src/components/common/Back.tsx
@@ -9,7 +9,7 @@ export default function Back() {
         className="flex items-center justify-center shadow-[0_2px_4px_0_rgba(56,57,56,0.2)] bg-white  p-0 w-[40px] h-[40px] rounded-full cursor-pointer"
         onClick={() => history.back()}
       >
-        <Image src="/images/icons/arrow/left.svg" className="w-6 h-10" alt="뒤로" />
+        <Image src="/images/icons/arrow/left.svg" width={24} height={40} alt="뒤로" />
       </button>
     </div>
   )
